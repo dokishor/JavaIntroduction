10b68605d49fa439754a31867bdca243a157430f
dokishor <okishor94@gmail.com>
2021-08-03 23:00:34 +0300

    complete task 3: find coordinates of historical monument

---
+++ src/main/java/org/hillel/HW2_work_with_variable/Coordinates.java
@@ -0,0 +1,15 @@
+package org.hillel.HW2_work_with_variable;
+
+public class Coordinates {
+
+    public static void main(String[] args) {
+
+        float nL = 46.200382f;
+        float eL = 30.349977f;
+
+        System.out.println("Координаты Аккерманской крепости: ");
+        System.out.println(" " + nL + " " + "с.ш.");
+        System.out.println(" " + eL + " " + "в.д.");
+
+    }
+}
