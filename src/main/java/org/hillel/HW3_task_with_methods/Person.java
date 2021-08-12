bcb00c07d624a099f113b76ebf6d32dde41ab2b9
dokishor <okishor94@gmail.com>
2021-08-12 20:51:34 +0300

    complete task: create methods which returned phone book

---
+++ src/main/java/org/hillel/HW3_task_with_methods/Person.java
@@ -0,0 +1,20 @@
+package org.hillel.HW3_task_with_methods;
+
+public class Person {
+
+    public static void main(String[] args) {
+
+        String personInfo = personInfo("Tom", "Cat", "London", 360245855);
+        System.out.println(personInfo);
+
+        String personInfo1 = personInfo("Jerry", "Mouse", "New York", 641246261);
+        System.out.println(personInfo1);
+
+        String personInfo2 = personInfo("Spike", "Dog", "Berlin", 896512568);
+        System.out.println(personInfo2);
+    }
+
+    public static String personInfo(String name, String surname, String city, int pNumb) {
+        return "Call a citizen: " + name + " " + surname + ", " + "from city: " + city + ", " + "you can call: " + pNumb;
+    }
+}
